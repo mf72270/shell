43bf42394cd2aa59498d66e9606c8f0ae2a6cd17
mf72270 <henaprint21@gmail.com>
2025-08-04 00:55:45 -0300

    updates

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -44,12 +44,18 @@ cd data
   #cp ./data/raw/*server*.log ./data/processed/server_logs/
 # 6. Repeat the above step for user logs and event logs
 cp  data/raw/*user*.log data/processed/user_logs/
-cp  data/raw/*event*.log data/processed/event_logs/
+    cp data/raw/*event*.log data/processed/event_logs/
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 #rm data/raw/*ipaddr* 2>/dev/null
 #rm data/processed/user_logs/*ipaddr* 2>/dev/null
+rm data/raw/*ipaddr*
+    rm data/processed/user_logs/*ipaddr* 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 #ls -R ./data/processed/ > ./data/inventory.txt
+
+ls data/processed/event_logs >data/inventory.txt
+    ls data/processed/server_logs >data/inventory.txt
+    ls data/processed/user_logs >data/inventory.txt
 
 ###########################################
 
